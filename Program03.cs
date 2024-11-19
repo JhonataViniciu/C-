227cddd1e75eeb871e0020b0c75d54f57594848d
Jhoww_24 <161961436+JhonataViniciu@users.noreply.github.com>
2024-11-19 01:34:33 -0300

    Add files via upload

--- Program03.cs
+++ Program03.cs
@@ -7,24 +7,19 @@ namespace program02
     {
         static void Main(string[] args)
         {
-            //definindo as variaveis//
-            char genero = 'F';
+
             int idade = 32;
             double saldo = 10.358768418;
             string nome = "maria";
 
-            //aprendendo as diferencas de saidas
-            Console.Write("Bom dia!"); 
-            Console.WriteLine("Boa tarde");
-            Console.WriteLine("Boa noite");
-            Console.WriteLine("---------------");
-            Console.WriteLine(genero);
-            Console.WriteLine(idade);
-            Console.WriteLine(saldo);
-            Console.WriteLine(nome);
-            Console.WriteLine(saldo.ToString("F2"));//delimitando as casas decimais
-            Console.WriteLine(saldo.ToString("F4"));
-            Console.WriteLine(saldo.ToString("F4", CultureInfo.InvariantCulture)); 
+            //Placeholders
+            Console.WriteLine("{0} tem {1} anos e tem saldo igual a {2:F2} reais",nome, idade, saldo);
+
+            //Interpolação
+            Console.WriteLine($"{nome} tem {idade} anos e tem saldo igual a {saldo:F2} reais");
+
+            //Concatenação 
+            Console.WriteLine(nome + " tem " + idade + " anos e tem saldo igual a " + saldo.ToString("F2", CultureInfo.InvariantCulture) + " reais");
         }
     }
 }
