64aa9e35911bcd33257ff187ae65995bfa3a8142
Jhoww_24 <161961436+JhonataViniciu@users.noreply.github.com>
2024-11-19 01:32:26 -0300

    Add files via upload

---
+++ Program03.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace program02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //definindo as variaveis//
+            char genero = 'F';
+            int idade = 32;
+            double saldo = 10.358768418;
+            string nome = "maria";
+
+            //aprendendo as diferencas de saidas
+            Console.Write("Bom dia!"); 
+            Console.WriteLine("Boa tarde");
+            Console.WriteLine("Boa noite");
+            Console.WriteLine("---------------");
+            Console.WriteLine(genero);
+            Console.WriteLine(idade);
+            Console.WriteLine(saldo);
+            Console.WriteLine(nome);
+            Console.WriteLine(saldo.ToString("F2"));//delimitando as casas decimais
+            Console.WriteLine(saldo.ToString("F4"));
+            Console.WriteLine(saldo.ToString("F4", CultureInfo.InvariantCulture)); 
+        }
+    }
+}
