174016fe3f7480b7a62617506f455f1c8223506b
Jhoww_24 <161961436+JhonataViniciu@users.noreply.github.com>
2024-11-20 23:16:13 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace Program05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 2;
+
+            double res = (double)a / b;
+            
+
+            Console.WriteLine(res);
+
+            float c = 2;
+            double d = (float)c;
+
+            Console.WriteLine(d);
+
+        }
+    }
+}
