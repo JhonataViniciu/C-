68f65dc896b3f913b57796b6008b950cd540743e
Jhoww_24 <161961436+JhonataViniciu@users.noreply.github.com>
2024-11-20 21:23:05 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,36 +1,32 @@
 ﻿using System;
 
-namespace PrimeiroProjeto
+namespace Program04
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            bool completo = false;
-            char genero = 'F';
-            char letra = '\u0041';
-            byte n1 = 126;
-            int n2 = 1000;
-            int n3 = 2147483647;
-            long n4 = 2147483648L;
-            float n5 = 4.5f;
-            double n6 = 4.5;
-            string nome = "Maria";
-            object obj1 = "Alex Brown";
-            object obj2 = 4.5f;
+            int a = 10;
 
-            Console.WriteLine(nome);
-            Console.WriteLine(letra);
-            Console.WriteLine(completo);
-            Console.WriteLine(genero);
-            Console.WriteLine(n1);
-            Console.WriteLine(n2);
-            Console.WriteLine(n3);
-            Console.WriteLine(n4);
-            Console.WriteLine(n5);
-            Console.WriteLine(n6);
-            Console.WriteLine(obj1);
-            Console.WriteLine(obj2);
+            Console.WriteLine(a);
+
+            a += 2;
+            Console.WriteLine(a);
+
+            a *= 3;
+            Console.WriteLine(a);
+
+            string s = "ABC";
+            Console.WriteLine(s);
+
+            s += "DEF";
+            Console.WriteLine(s);
+
+            int b = 10;
+            int c = ++b;
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+
         }
     }
 }
