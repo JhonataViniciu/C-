0c3a5684a9fb9e7bdf020f233f7dc8caa759ab20
Jhoww_24 <161961436+JhonataViniciu@users.noreply.github.com>
2024-11-25 10:20:55 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Program11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Estrutura Condicional
+            Console.WriteLine("Entre com um numero inteiro: ");
+            int x = int.Parse(Console.ReadLine());
+
+            if (x % 2 == 0) {
+                Console.WriteLine("Par");
+            }
+            else {
+                Console.WriteLine("Impar");
+            }
+
+
+            //Estrutura Encadeamentos
+            Console.WriteLine("Qual a hora atual?");
+            int hora = int.Parse(Console.ReadLine());
+
+            if(hora < 12) {
+                Console.WriteLine("Bom dia");
+            } 
+            else if (hora < 18) {
+                Console.WriteLine("Boa Tarde");
+            } 
+            else {
+                Console.WriteLine("Boa Noite");
+            }
+        }
+    }
+}
