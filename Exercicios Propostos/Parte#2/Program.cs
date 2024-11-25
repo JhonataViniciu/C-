e6691e7ca7bc5a082c8487974cc6dfcdb1740981
Jhoww_24 <161961436+JhonataViniciu@users.noreply.github.com>
2024-11-25 11:18:25 -0300

    Add files via upload

---
+++ Exercicios Propostos/Parte#2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exercicio06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com um numerp inteiro: ");
+            int x = int.Parse(Console.ReadLine());
+
+            if (x < 0) {
+                Console.WriteLine("NEGATIVO");
+            }
+            else {
+                Console.WriteLine("NAO NEGATIVO");
+            }
+        }   
+    }
+}
