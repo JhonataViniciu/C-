551d2e20cbfcf73e3bc1362f3a1a9b1525369099
Jhoww_24 <161961436+JhonataViniciu@users.noreply.github.com>
2024-11-25 11:14:22 -0300

    Add files via upload

---
+++ Exercicios Propostos/Parte#1/Program.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+using System.Globalization;
+
+
+namespace Exercicios_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual o codigo da peca? ");
+            int cod = int.Parse(Console.ReadLine());
+            Console.WriteLine("Quantas pecas serao? ");
+            int qnt = int.Parse(Console.ReadLine());
+            Console.WriteLine("Qual o valor da peca? ");
+            double valor = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Qual o codigo da peca? ");
+            int cod2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Quantas pecas serao? ");
+            int qnt2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Qual o valor da peca? ");
+            double valor2 = double.Parse(Console.ReadLine());
+
+            double valor_total = qnt * valor;
+            double valor_total2 = qnt2 * valor2;
+            double res = valor_total + valor_total2;
+            Console.WriteLine("VALOR TOTAL A PAGAR = " + res.ToString("F2"));
+        }
+    }
+}
