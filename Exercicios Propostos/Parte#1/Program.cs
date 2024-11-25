2a6632b203df931dfe4d5ada4573e3f5de9954b6
Jhoww_24 <161961436+JhonataViniciu@users.noreply.github.com>
2024-11-25 11:08:56 -0300

    Add files via upload

---
+++ Exercicios Propostos/Parte#1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace Exercicios_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int res = num1 + num2;
+            Console.WriteLine(res);
+        }
+    }
+}
