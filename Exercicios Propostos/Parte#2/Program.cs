6cb28f1da41866f89f65b1e73c4c2dd528076931
Jhoww_24 <161961436+JhonataViniciu@users.noreply.github.com>
2024-11-27 17:02:14 -0300

    Add files via upload

---
+++ Exercicios Propostos/Parte#2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Exercicio08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um valor: ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite outro valor : ");
+            int b = int.Parse(Console.ReadLine());
+
+            int c = a % 3;
+            int d = b % 3;
+
+            if (c == 0 &&  d == 0) {
+                Console.WriteLine("Sao Multiplos");
+            }
+            else {
+                Console.WriteLine("Nao sao Multiplos");
+            }
+        }
+    }
+}
