a47ba4d6d33bea9483d596314331a79c57a2cff2
Jhoww_24 <161961436+JhonataViniciu@users.noreply.github.com>
2024-11-27 16:52:44 -0300

    Add files via upload

---
+++ Exercicios Propostos/Parte#2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exercicio07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um numero: ");
+            int x = int.Parse(Console.ReadLine());
+
+            if (x % 2 == 0) {
+                Console.WriteLine("O numero é par");
+            }
+            else {
+                Console.WriteLine("O numero é impar");
+            }
+        }
+    }
+}
