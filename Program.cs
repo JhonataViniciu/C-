7a558bc80179a6b945ceb0d18d99dc868a0bb502
Jhoww_24 <161961436+JhonataViniciu@users.noreply.github.com>
2024-11-29 09:58:55 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Program14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Quantos numeros inteiros voce vai digitar? :");
+            int N = int.Parse(Console.ReadLine());
+
+            int soma = 0;
+            for (int i = 1; i <= N; i++) {
+                Console.Write("valor #{0}: ", i);
+                int valor = int.Parse(Console.ReadLine());
+                soma += valor;
+            }
+            Console.WriteLine("soma = "+ soma);
+        }
+    }
+}
