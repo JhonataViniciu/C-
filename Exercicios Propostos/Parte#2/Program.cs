e61a38e73853844be7ccd31b541d4c20afa1a4c9
Jhoww_24 <161961436+JhonataViniciu@users.noreply.github.com>
2024-11-29 10:18:49 -0300

    Add files via upload

---
+++ Exercicios Propostos/Parte#2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exercicio10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int senha;
+            Console.Write("Digite sua senha (4 digitos): ");
+            senha = int.Parse(Console.ReadLine());
+
+            while (senha != 2020) {
+                Console.Write("Senha invalida. \n  Digite novamente: ");
+                senha = int.Parse(Console.ReadLine());
+            } 
+            Console.WriteLine("Acesso Permitido!");
+        }
+    }
+}
