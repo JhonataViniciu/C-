edda3e7761a9fe33ee3fad2b6dc91966368feaf4
Jhoww_24 <161961436+JhonataViniciu@users.noreply.github.com>
2024-12-03 16:19:23 -0300

    Add files via upload

---
+++ Triangulo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Program15
+{
+    internal class Triangulo
+    {
+        public double A;
+        public double B;
+        public double C;
+
+        public double Area();
+    }
+}
