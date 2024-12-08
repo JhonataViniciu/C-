6bd614309e0ab911357bcd843ef603811ea13a20
Jhoww_24 <161961436+JhonataViniciu@users.noreply.github.com>
2024-12-08 14:39:23 -0300

    Add files via upload

---
+++ Produtos.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+
+namespace Program16
+{
+    internal class Produtos
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double ValorToatlEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+        
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+
+        public override string ToString()
+        {
+            return Nome 
+                + ", $ " 
+                + Preco.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + Quantidade 
+                + " unidades, Total: $ "
+                + ValorToatlEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
