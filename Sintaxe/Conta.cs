69de504baae1cc08c1ad719c17ee746723ccbbbb
Jhoww_24 <161961436+JhonataViniciu@users.noreply.github.com>
2024-12-19 14:59:20 -0300

    Add files via upload

--- Sintaxe/Conta.cs
+++ Sintaxe/Conta.cs
@@ -1,15 +1,36 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Globalization;
 
 namespace Conta_Bancaria
 {
     internal class Program
     {
+        
         static void Main(string[] args)
         {
+            
+            DadosBancario conta;
+            
+            Console.Write("Entre com o numero da Conta: ");
+            int numero = int.Parse(Console.ReadLine());
+            Console.Write("Entre com o Titular da conta: ");
+            string titular = Console.ReadLine();
+            Console.Write("Havera deposito inicial (s/n)? ");
+            char resp = char.Parse(Console.ReadLine());
+            if (resp == 's' || resp == 'S')
+            {
+                Console.Write("Entre com o valor de deposito inicial: ");
+                double depositoInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                conta = new DadosBancario(numero, titular, depositoInicial);
+            }
+            else
+            {
+                conta = new DadosBancario(numero, titular);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta: ");
+            Console.WriteLine(conta);
         }
     }
 }
--- Sintaxe/DadosBancario.cs
+++ Sintaxe/DadosBancario.cs
@@ -1,12 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using System.Globalization;
 
 namespace Conta_Bancaria
 {
     internal class DadosBancario
     {
+
+        public int Numero { get; private set; }
+        public string Titular { get; set; }
+        public double Saldo { get; private set; }
+        
+        public DadosBancario(int numero, string titular)
+        {
+            Numero = numero;
+            Titular = titular;
+        }
+
+        public DadosBancario(int numero, string titular, double saldo) : this(numero, titular)
+        {
+            Saldo = saldo;
+        }
+
+        public override string ToString()
+        {
+            return "Conta "
+                + Numero
+                + ", Titular: "
+                + Titular 
+                + ", Saldo: $ "
+                + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
     }
 }
