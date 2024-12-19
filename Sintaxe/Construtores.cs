3c3cb69f5f24e109ecbf2e91f2bf9846b98c8498
Jhoww_24 <161961436+JhonataViniciu@users.noreply.github.com>
2024-12-19 14:01:36 -0300

    Add files via upload

--- Sintaxe/Construtores.cs
+++ Sintaxe/Construtores.cs
@@ -7,36 +7,12 @@ namespace Construtores
     {
         static void Main(string[] args)
         {
-      
-            Console.WriteLine("Entre com os dados do produto: ");
-            Console.Write("Nome: ");
-            string nome = Console.ReadLine();
-            Console.Write("Preco: ");
-            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            Console.Write("Quantidade: ");
-            int quantidade = int.Parse(Console.ReadLine());
 
-            Produtos p = new Produtos(nome, preco, quantidade);
+            Produtos p = new Produtos("Tv", 500.00);
 
-            Console.WriteLine();
-            Console.Write("Dados do produto: " + p);
+            p.SetNome("Tv 4k");
 
-            Console.WriteLine();
-            Console.WriteLine("Digite o numero de produtos a ser adicionado: ");
-            int qtd = int.Parse(Console.ReadLine());
-            p.AdicionarProdutos(qtd);
-
-            Console.WriteLine();
-            Console.WriteLine("Dados atualizados: " + p);
-
-            Console.WriteLine();
-            Console.WriteLine("Digite o numero de produtos a ser removido: ");
-            qtd = int.Parse(Console.ReadLine());
-            p.RemoverProdutos(qtd);
-
-            Console.WriteLine();
-            Console.WriteLine("Dados atualizados: " + p);
-            
+            Console.WriteLine(p.GetNome());
         }
     }
 }
--- Sintaxe/ConstrutoresClass.cs
+++ Sintaxe/ConstrutoresClass.cs
@@ -6,39 +6,60 @@ namespace Construtores
 {
     internal class Produtos
     {
-        public string Nome;
-        public double Preco;
-        public int Quantidade;
+        private string _nome;
+        private double _preco;
+        private int _quantidade;
 
-        public Produtos(string nome, double preco, int quantidade)
-        {
-            Nome = nome;
-            Preco = preco;
-            Quantidade = quantidade;
+        public Produtos() { 
+        }
+        public Produtos(string nome, double preco) : this() {
+            _nome = nome;
+            _preco = preco;
+        }
+        public Produtos(string nome, double preco, int quantidade) : this(nome, preco) {
+            _quantidade = quantidade;
         }
 
+        public string GetNome() { 
+        return _nome;
+        }
+
+        public void SetNome(string nome) {
+            if (nome != null && nome.Length > 1)
+            {
+                _nome = nome;
+            }
+        }
+
+        public double GetPreco() {
+            return _preco;
+        }
+
+        public int GetQuantidade() {
+            return _quantidade;
+        }
         public double ValorToatlEmEstoque()
         {
-            return Preco * Quantidade;
+            return _preco * _quantidade;
         }
 
         public void AdicionarProdutos(int quantidade)
         {
-            Quantidade += quantidade;
+            _quantidade += quantidade;
         }
 
         public void RemoverProdutos(int quantidade)
         {
-            Quantidade -= quantidade;
+            _quantidade -= quantidade;
         }
 
         public override string ToString()
         {
-            return Nome
+            return _nome
                 + ", $ "
-                + Preco.ToString("F2", CultureInfo.InvariantCulture)
+                + _preco.ToString("F2", CultureInfo.InvariantCulture)
                 + ", "
-                + Quantidade
+                + _quantidade
                 + " unidades, Total: $ "
                 + ValorToatlEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
         }
