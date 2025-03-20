0d687f18da0f1cfac2f88485cc706c8cfb41af3c
Jhoww_24 <161961436+JhonataViniciu@users.noreply.github.com>
2025-03-20 15:41:52 -0300

    Add files via upload

---
+++ Condicional Ternaria.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+namespace Inferencia_de_Tipos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double desconto = (preco < 20.0 ? preco * 0.1 : preco * 0.05);
+
+            
+
+            Console.WriteLine(desconto);
+
+        }
+    }
+}
