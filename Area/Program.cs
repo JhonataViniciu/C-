e588f13bdda4ea0fb2ce37bbb9bb4eff89eaddc7
Jhoww_24 <161961436+JhonataViniciu@users.noreply.github.com>
2025-04-15 14:32:29 -0300

    Add files via upload

--- Area/Program.cs
+++ Area/Program.cs
@@ -1,1 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace Area
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
