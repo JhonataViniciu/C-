5596c15d0e00764498031c6d15ee854ed91fc961
Jhoww_24 <161961436+JhonataViniciu@users.noreply.github.com>
2025-04-15 14:33:00 -0300

    Add files via upload

---
+++ Area/Entities/Circle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Area.Entities.Enums;
+
+
+namespace Area.Entities
+{
+    internal class Circle : Shape
+    {
+        public double Radius { get; set; }
+
+        public Circle(double radius, Color color) : base(color)
+        {
+            Radius = radius;
+        }
+
+        public override double Area()
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+}
---
+++ Area/Entities/Shape.cs
@@ -0,0 +1,16 @@
+﻿using Area.Entities.Enums;
+
+namespace Area.Entities
+{
+    abstract internal class Shape
+    {
+        public Color Color { get; set; }
+
+        public Shape(Color color)
+        {
+            Color = color;
+        }
+
+        public abstract double Area();
+    }
+}
