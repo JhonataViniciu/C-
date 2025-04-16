3d6f38b3c019975e3aab48bf63390853854c4a58
Jhoww_24 <161961436+JhonataViniciu@users.noreply.github.com>
2025-04-16 16:22:55 -0300

    Add files via upload

---
+++ Exeções/Entities/Reservations.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Try_catch.Entities
+{
+    internal class Reservations
+    {
+        public int RoomNumber { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+
+        public Reservations() { }
+
+        public Reservations(int roomNumber, DateTime checkIn, DateTime checkOut)
+        {
+            RoomNumber = roomNumber;
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+        public int Duration()
+        {
+            TimeSpan duration = CheckOut.Subtract(CheckIn);
+            return (int)duration.TotalDays;
+        }
+
+        public void UpdateDates(DateTime chackIn, DateTime chackOut)
+        {
+            CheckIn = chackIn;
+            CheckOut = chackOut;
+        }
+
+        public override string ToString()
+        {
+            return "Room"
+                + RoomNumber
+                + ", check-in: "
+                + CheckIn.ToString("dd/MM/yyy")
+                + ", check-out: "
+                + CheckOut.ToString("dd/MM/yyyy")
+                + ", "
+                + Duration()
+                + " nigths";
+        }
+    }
+}
