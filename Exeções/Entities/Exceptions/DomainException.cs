7a241eb064709a0cf2738a7f502badab653dbce6
Jhoww_24 <161961436+JhonataViniciu@users.noreply.github.com>
2025-04-22 10:58:27 -0300

    Add files via upload

--- Exeções/Entities/Exceptions/DomainException.cs
+++ Exeções/Entities/Exceptions/DomainException.cs
@@ -1,1 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace Try_catch.Entities
+{
+    internal class DomainException : ApplicationException
+    {
+        public DomainException(string message) : base(message)
+        {
+        }
+    }
+}
