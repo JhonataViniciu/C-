3c1aa9e6919936975466f3e33975ca2460ba6b5f
Jhoww_24 <161961436+JhonataViniciu@users.noreply.github.com>
2025-05-14 15:17:27 -0300

    Add files via upload

---
+++ Arquivos/DirectoryInfo.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+
+namespace execoes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\temp\MyFolders";
+
+
+            //Listar apartir de uma pasta informada
+            try
+            {
+                IEnumerable<string> folders = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("FOLDERS: ");
+                foreach (string s in folders)
+                {
+                    Console.WriteLine(s);
+                }
+
+
+                //Listar os arquivos
+                IEnumerable<string> files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("FILES: ");
+                foreach (string s in files)
+                {
+                    Console.WriteLine(s);
+                }
+
+                //Criar Pastas
+                Directory.CreateDirectory(path + @"\newFoler");
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+
+
+        }
+    }
+}
