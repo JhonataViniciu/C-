509e20eedbf24ba71b6503151f0739755effa895
Jhoww_24 <161961436+JhonataViniciu@users.noreply.github.com>
2025-07-08 14:13:18 -0300

    Add files via upload

---
+++ Interfaces/Solução Interfaces/Services/RentalService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Interfaces.Entities;
+namespace Interfaces.Services
+{
+    internal class RentalService
+    {
+        public double PricePerHour { get; set; }
+        public double PricePerDay { get; set; }
+
+        public RentalService(double pricePerHour, double pricePerDay)
+        {
+            PricePerHour = pricePerHour;
+            PricePerDay = pricePerDay;
+        }
+
+        public void ProcessInvoice(CarRental carRental)
+        {
+
+        }
+    }
+}
