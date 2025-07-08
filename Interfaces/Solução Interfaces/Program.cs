7887f94fa05a88c9dcd98aedaeff35f6c03ad739
Jhoww_24 <161961436+JhonataViniciu@users.noreply.github.com>
2025-07-08 15:04:53 -0300

    Add files via upload

--- Interfaces/Solução Interfaces/Program.cs
+++ Interfaces/Solução Interfaces/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Globalization;
 using Interfaces.Entities;
+using Interfaces.Services;
 
 namespace Interfaces
 {
@@ -13,14 +14,25 @@ namespace Interfaces
         static void Main(string[] args)
         {
             Console.WriteLine("Enter rental data: ");
-            Console.WriteLine("Car Model");
+            Console.Write("Car Model");
             string model = Console.ReadLine();
-            Console.WriteLine("Pickup (dd/MM/yyyy hh:mm): ");
+            Console.Write("Pickup (dd/MM/yyyy hh:mm): ");
             DateTime start = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm",CultureInfo.InvariantCulture);
-            Console.WriteLine("Return (dd/MM/yyyy hh:mm): ");
+            Console.Write("Return (dd/MM/yyyy hh:mm): ");
             DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
 
+            Console.Write("Enter price per hour: ");
+            double hour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Enter price per day: ");
+            double day = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
             CarRental carRental = new CarRental(start, finish, new Vehicle(model));
+
+            RentalService rentalService = new RentalService(hour, day);
+            rentalService.ProcessInvoice(carRental);
+
+            Console.Write("INVOICE: ");
+            Console.Write(carRental.Invoice);
         }
     }
 }
