597c477cb1472cf0afbe26188ce9841fa2abef3a
Jhoww_24 <161961436+JhonataViniciu@users.noreply.github.com>
2025-07-10 14:43:12 -0300

    Add files via upload

--- Generics/Reuso/Program.cs
+++ Generics/Reuso/Program.cs
@@ -7,7 +7,7 @@ namespace Generic
         static void Main(string[] args)
         {
 
-            PrintService printService = new PrintService();
+            PrintService<int> printService = new PrintService<int>();
 
             Console.Write("How Many values? ");
             int n = int.Parse(Console.ReadLine());
