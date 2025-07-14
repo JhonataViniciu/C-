238e40db64949127f12d54bf9a98474efbff2f24
Jhoww_24 <161961436+JhonataViniciu@users.noreply.github.com>
2025-07-14 13:05:41 -0300

    Add files via upload

--- Generics/HashSet and SortedSet/Program.cs
+++ Generics/HashSet and SortedSet/Program.cs
@@ -1,1 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
 
+namespace HashSet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> set = new HashSet<string>();
+
+            set.Add("Maria");
+            set.Add("Alex");
+
+            Console.WriteLine(set.Contains("Maria"));
+        }
+
+    }
+}
