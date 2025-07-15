e10b686bf12009aa83331671830681fefb1e7486
Jhoww_24 <161961436+JhonataViniciu@users.noreply.github.com>
2025-07-15 10:38:54 -0300

    Add files via upload

--- Generics/Dictionary and SortedDictionary/Program.cs
+++ Generics/Dictionary and SortedDictionary/Program.cs
@@ -1,1 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Dictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> cookies =  new Dictionary<string, string>();
 
+            cookies["user"] = "maria";
+            cookies["email"] = "maria@gmail.com";
+            cookies["telefone"] = "11995095434";
+            cookies["telefone"] = "11930220409";
+
+
+            Console.WriteLine(cookies["email"]);
+            Console.WriteLine(cookies["telefone"]);
+
+            cookies.Remove("email");
+
+            if (cookies.ContainsKey("email"))
+            {
+                Console.WriteLine(cookies["email"]);
+            }
+            else
+            {
+                Console.WriteLine("There is no 'email' key");
+            }
+            Console.WriteLine("Size: " + cookies.Count);
+
+            Console.WriteLine("ALL COOKIES: ");
+            foreach(var item in cookies)
+            {
+                Console.WriteLine(item.Key + ": " + item.Value);
+            }
+
+        }
+    }
+}
