210be06218cfba099b75021f94996a2f3bae1651
Jhoww_24 <161961436+JhonataViniciu@users.noreply.github.com>
2025-07-16 10:02:30 -0300

    Add files via upload

--- Delegates/Service/CalculationService.cs
+++ Delegates/Service/CalculationService.cs
@@ -1,1 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
 
+namespace Delegates.Services
+{
+    internal class CalculationService
+    {
+        public static double Max(double x, double y)   //Retonar o maior deles
+        {
+            return (x > y) ? x : y;
+        }
+        public static double Sum(double x, double y)   //Retorna a soma deles
+        {
+            return x + y;
+        }
+
+        public static double Square(double x)          //Retorna o Quadrado dele
+        {
+            return x * x;
+        }
+    }
+}
