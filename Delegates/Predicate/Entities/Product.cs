f5cc139bba863eaa9a2f989ad40474638df176ed
Jhoww_24 <161961436+JhonataViniciu@users.noreply.github.com>
2025-07-16 10:23:58 -0300

    Add files via upload

--- Delegates/Predicate/Entities/Product.cs
+++ Delegates/Predicate/Entities/Product.cs
@@ -1,1 +1,21 @@
+﻿using System;
 
+namespace Predicate.Entities
+{
+    internal class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return Name + ", " + Price.ToString("F2");
+        }
+    }
+}
