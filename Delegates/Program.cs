bc83093b188e15cf2bc80abdb4d475315051fa18
Jhoww_24 <161961436+JhonataViniciu@users.noreply.github.com>
2025-07-16 10:02:48 -0300

    Add files via upload

--- Delegates/Program.cs
+++ Delegates/Program.cs
@@ -1,1 +1,22 @@
+﻿using System;
+using Delegates.Services;
 
+namespace Delegates
+{
+
+    delegate double BinaryNumericOperation(double n1, double n2);  //criando um tipo referencial
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+
+            BinaryNumericOperation op = CalculationService.Sum;  //introduzindo a referencia
+
+            double resul = op.Invoke(a, b);
+            Console.WriteLine(resul);
+        }
+    }
+}
