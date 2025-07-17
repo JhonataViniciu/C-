5922bd5117ac2a3f9b8a6d4c18fca1954f5a000b
Jhoww_24 <161961436+JhonataViniciu@users.noreply.github.com>
2025-07-17 10:25:45 -0300

    Add files via upload
    Introdução ao LINQ

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,1 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
+namespace Linq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Especificar o data source
+            int[] numbers = new int[] { 1, 2, 3, 4, 5 };
+
+            //Define a expressao query
+            var result = numbers.Where(x => x % 2 == 0).Select(x => x * 10);
+
+            //Executar a consulta
+            foreach ( int x in result)
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
